da7e12674c7e5a7deba36474ffe7ecd5be3f598e
camilaViquez <32073514+camilaViquez@users.noreply.github.com>
2017-09-28 22:58:58 -0600

    primer commit

---
+++ WindowsApp2/WindowsApp2/Concatenacion (join).vb
@@ -0,0 +1,32 @@
+﻿Public Class Concatenacion__join_
+    Public tab1
+    Public tab2
+    Public pred
+    Public valores
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles predicado.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        tab1 = tabla1.Text
+        tab2 = tabla2.Text
+        pred = predicado.Text
+
+        'Select * from  EMPLEADOS join CLIENTES on( EMPLEADOS.id_canton = CLIENTES.id_canton)
+        valores = " Select *from " + tab1 + " join " + tab2 + " on " + tab1 + "." + pred + " = " + tab2 + "." + pred
+        LabelSQL.Text = " Select *from " + tab1 + " join " + tab2 + " on " + tab1 + "." + pred + " = " + tab2 + "." + pred
+        LabelA.Text = " π " + pred + "(" + tab1 + ")" + " U " + " π " + pred + "(" + tab2 + ")"
+        'MessageBox.Show(valoresU)
+        'SqlCommand Command() = New SqlCommand("select count(*) from" + x, cn)
+        llenarDatagridviewConcatenacion(DataGridView2, valores)
+
+
+
+
+
+    End Sub
+End Class
---
+++ WindowsApp2/WindowsApp2/Module1.vb
@@ -0,0 +1,75 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.Sql
+
+
+
+Module Module1
+    Public cn As SqlConnection
+    Public adaptador As SqlDataAdapter
+    Public dt As DataTable
+    Public x
+
+    Sub abrirConexion()
+        Try
+            cn = New SqlConnection("Data Source=MATILDA;Initial Catalog=Ventas;Integrated Security=True")
+            cn.Open()
+            MessageBox.Show("Se realizo la coneccion con exito")
+
+        Catch ex As Exception
+            MessageBox.Show(" No se pudo conectar" + ex.ToString)
+
+
+        End Try
+    End Sub
+
+
+    Sub llenarDatagridviewSeleccion(ByVal dgv As DataGridView, ByVal parametros As String)
+
+        Try
+
+
+            adaptador = New SqlDataAdapter(parametros, cn)
+            dt = New DataTable
+            adaptador.Fill(dt)
+            dgv.DataSource = dt
+
+
+
+        Catch ex As Exception
+            MessageBox.Show("No se lleno el datagridView debido a: " + ex.ToString)
+
+        End Try
+    End Sub
+
+    Sub llenarDatagridviewProyeccion(ByVal dgv As DataGridView, ByVal parametros As String)
+
+        Try
+
+
+            adaptador = New SqlDataAdapter(parametros, cn)
+            dt = New DataTable
+            adaptador.Fill(dt)
+            dgv.DataSource = dt
+
+
+
+        Catch ex As Exception
+            MessageBox.Show("No se lleno el datagridView debido a: " + ex.ToString)
+
+        End Try
+    End Sub
+
+    Public Sub llenarDatagridviewConcatenacion(ByVal dgv As DataGridView, ByVal parametrosU As String)
+        Try
+            adaptador = New SqlDataAdapter(parametrosU, cn)
+            dt = New DataTable
+            adaptador.Fill(dt)
+            dgv.DataSource = dt
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+End Module
